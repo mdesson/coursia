776a491e7c54e3e0be6bd102863141494d233829
mdesson <michael.desson@gmail.com>
2019-01-15 09:41:40 -0500

    Added JSON for mapping courseIDs to course subject and catalog

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 // load API keys and local settings
 let rawdata = fs.readFileSync('config.json');  
 let config = JSON.parse(rawdata);  
+let id = JSON.parse(fs.readFileSync('./data/courseids.json', 'utf8'));
 
 // Set up app
 app.use(express.static('public'))
@@ -22,9 +23,9 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     let subject = req.body.subject
     let catalog = req.body.catalog
-    let career, courseid = "*"
+    let courseid = id.filter(course => course.subject == subject && course.catalog == catalog)[0].ID
     let sched_url = `https://opendata.concordia.ca/API/v1/course/schedule/filter/${courseid}/${subject}/${catalog}`
-    let cat_url = `https://opendata.concordia.ca/API/v1/course/catalog/filter/${subject}/${catalog}/${career}`
+    let cat_url = `https://opendata.concordia.ca/API/v1/course/catalog/filter/${subject}/${catalog}/*`
     let auth = "Basic " + new Buffer(config.apiUser + ":" + config.apiKey).toString("base64");
     request( {url: cat_url, headers: {"Authorization": auth}}, function (err, response, body) {
         let courses = JSON.parse(body)
