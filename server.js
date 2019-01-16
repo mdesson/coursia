d1877f0ab8395b3373bac552c1d43f71647751f3
mdesson <mdesson@gmail.com>
2019-01-16 00:40:34 -0500

    Cleaned up sched_url to remove useless search terms

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/', function (req, res) {
     let courseid = id.filter(course => course.subject == subject && course.catalog == catalog)[0].ID
     
     // request URLs for open data portal
-    let sched_url = `https://opendata.concordia.ca/API/v1/course/schedule/filter/${courseid}/${subject}/${catalog}`
+    let sched_url = `https://opendata.concordia.ca/API/v1/course/schedule/filter/${courseid}/*/*`
     let cat_url = `https://opendata.concordia.ca/API/v1/course/catalog/filter/${subject}/${catalog}/*`
     let desc_url = `https://opendata.concordia.ca/API/v1/course/description/filter/${courseid}`
     
