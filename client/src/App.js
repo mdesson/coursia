1c840f28445893926b99baaa6eb19cfcae2c82b5
Mike <michael.desson@gmail.com>
2019-07-28 12:29:05 -0400

    Rebuilt backend skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
 
 function App() {
+  const [testText, setTestText] = useState('inital')
+
+  const serverGET = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/test')
+      setTestText(response.data.hello)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={serverGET} className="App">
+      <p>{testText}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
